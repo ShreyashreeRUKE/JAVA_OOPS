2b03b67e4940dda99b37fab61fedbb0155cb0c2a
Shreyashree Banerjee <147500684+ShreyashreeRUKE@users.noreply.github.com>
2025-04-17 23:10:25 +0530

    AbstractionDemo.java

--- AbstractionDemo.java
+++ AbstractionDemo.java
@@ -1,22 +1,28 @@
-package OOPS;
-abstract class Shape{
-	public abstract void area(double r);
+package OOPS; //This is kind of folder 
+abstract class Shape //Creating an Abstruct class 
+{
+	public abstract void area(double r); //Abstruct method with no body 
 }
- class Squre extends Shape{
-	public  void area(double r) {
+ class Squre extends Shape //This class inherits from Shape class
+ {
+	public  void area(double r) //Overrides area()
+	{
 		System.out.println("Area squre "+(r*r));
 	}
 }
 class Circle extends Shape{
 	public void area(double r) {
-		System.out.println("Area of circle "+Math.PI*r*r);
+		System.out.println("Area of circle "+Math.PI*r*r); //Math.PI is a build in const of val 3.14159
 	}
 }
-public class AbstractionDemo {
+public class AbstractionDemo //Main class and it's public means it can access by the JVM directly
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) //Its always void cause it's nott returning anything
+					//It's static cause JVM has to access it without making an object
+	{
 		// TODO Auto-generated method stub
-		Squre sq= new Squre();
+		Squre sq= new Squre(); //Create an instance object of the class 
 		Circle c= new Circle();
 		sq.area(5.6);
 		c.area(5.6);

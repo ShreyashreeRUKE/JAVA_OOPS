c5297d43c99bce1cf58b75e6a1f336fcf8fe9a06
Shreyashree Banerjee <147500684+ShreyashreeRUKE@users.noreply.github.com>
2025-04-18 07:10:45 +0530

    ConstractorOverloadingDemo.java

--- ConstractorOverloadingDemo.java
+++ ConstractorOverloadingDemo.java
@@ -1,9 +1,13 @@
-package OOPS;
-class Cake{
-	public Cake() {
+//Constructor overloading ... same constructors name with diff parameters in it
+package OOPS;//Folder
+class Cake 
+{
+	public Cake() //Constructor with no parameters
+	{
 		System.out.println("Happy Birthday");
 	}
-	public Cake(String msg, String shape){
+	public Cake(String msg, String shape) //Constuctor with two parameters msg and shape
+	{
 		System.out.println(msg);
 		System.out.println(shape);
 	}
@@ -13,12 +17,14 @@ class Cake{
 		System.out.println(price);
 	}
 }
-public class ConstractorOverloadingDemo {
-
-	public static void main(String[] args) {
-
-		Cake c1=new Cake();
-		Cake c2=new Cake("Happy Birthday", "Round");
+public class ConstractorOverloadingDemo //Main class
+{
+	public static void main(String[] args) //Main method
+	{
+		Cake c1=new Cake(); //Cake is the class..c1 in new obj that i'm creating
+				//new is keyword that makes the new obj and calss the cake constructor ..
+				// cake() is the constructor
+		Cake c2=new Cake("Happy Birthday", "Round"); //two parameters need to be passed
 		Cake c3=new Cake("Nice","Circle", 100.50);
 	}
 
